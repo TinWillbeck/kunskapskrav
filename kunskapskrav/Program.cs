f907327540289e678c340d0b60d350a29e282066
Tin Willbeck <tin.willbeck@elev.ga.ntig.se>
2023-03-30 16:06:09 +0200

    fgdsa

---
+++ kunskapskrav/Program.cs
@@ -0,0 +1,20 @@
+﻿bool running = true;
+
+while (running == true)
+{
+    Console.WriteLine("Skriv ett tal");
+    string numTxt = Console.ReadLine();
+    float   numFloat;
+    bool attempt = float.TryParse(numTxt, out numFloat);
+
+    if (attempt == true){
+        Console.WriteLine("Bra jobbat, du skrev ett tal!");
+        Console.WriteLine($"Du skrev talet {numFloat}");
+        Console.WriteLine("Klicka enter för att avsluta");
+        Console.ReadLine();
+        running = false;
+    }
+    else{
+        Console.WriteLine("Det där var inte ett tal, försök igen!");
+    }
+}
